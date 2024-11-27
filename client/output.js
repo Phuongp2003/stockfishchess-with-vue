da287e340613e37ee4951a38c837af53f15b5e93
Phuongp2003 <phambuinamhphuong@gmail.com>
2024-11-28 04:09:05 +0700

    up to v0.8.6

--- client/output.js
+++ client/output.js
@@ -2,13 +2,15 @@ import 'vue3-chessboard/style.css';
 
 import ChessBoard from '@/components/ChessBoard.vue';
 import ChessBoardServerless from '@/components/ChessBoardServerless.vue';
+import PVPChessboard from '@/components/PVPBoard.vue';
 
 const Chess = {
     install(Vue) {
         Vue.component(ChessBoard);
         Vue.component(ChessBoardServerless);
+        Vue.component(PVPChessboard);
     }
 }
 
 export default Chess;
-export { ChessBoard, ChessBoardServerless };
+export { ChessBoard, ChessBoardServerless, PVPChessboard };
